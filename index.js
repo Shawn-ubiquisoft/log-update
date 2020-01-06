851afbf4f4cf9e3a9065d981a4b44ddeac7a20ab
Shawn Mercado <38562593+Shawn-ubiquisoft@users.noreply.github.com>
2020-01-06 17:06:40 -0600

    fix issue with re-renders overflowing the terminal

--- index.js
+++ index.js
@@ -4,55 +4,88 @@ const cliCursor = require('cli-cursor');
 const wrapAnsi = require('wrap-ansi');
 
 const getWidth = stream => {
-	const {columns} = stream;
+  const {columns} = stream;
 
-	if (!columns) {
-		return 80;
-	}
+  if (!columns) {
+    return 80;
+  }
 
-	return columns;
+  // Windows appears to wrap a character early
+  // I hate Windows so much
+  if (process.platform === 'win32') {
+    return columns - 1;
+  }
+
+  return columns;
+};
+
+const getHeight = stream => {
+  const {rows} = stream;
+
+  if (!rows) {
+    return 20;
+  }
+
+  return rows;
+};
+
+const cropLinesFromFront = (text, noOfLines) => {
+  const lines = text.split('\n');
+  return lines.slice(noOfLines).join('\n');
+};
+
+const fitToTerminalHeight = (stream, text) => {
+  const terminalHeight = getHeight(stream);
+  const lineCount = text.split('\n').length;
+
+  if (lineCount > terminalHeight) {
+    return cropLinesFromFront(text, (lineCount - terminalHeight));
+  }
+
+  return text;
 };
 
 const main = (stream, options) => {
-	options = Object.assign({
-		showCursor: false
-	}, options);
+  options = Object.assign({
+    showCursor: false
+  }, options);
 
-	let prevLineCount = 0;
+  let prevLineCount = 0;
 
-	const render = (...args) => {
-		if (!options.showCursor) {
-			cliCursor.hide();
-		}
+  const render = (...args) => {
+    if (!options.showCursor) {
+      cliCursor.hide();
+    }
 
-		let out = args.join(' ') + '\n';
-		out = wrapAnsi(out, getWidth(stream), {
-			trim: false,
-			hard: true,
-			wordWrap: false
-		});
-		stream.write(ansiEscapes.eraseLines(prevLineCount) + out);
-		prevLineCount = out.split('\n').length;
-	};
+    let out = args.join(' ') + '\n';
+    out = fitToTerminalHeight(stream, out);
 
-	render.clear = () => {
-		stream.write(ansiEscapes.eraseLines(prevLineCount));
-		prevLineCount = 0;
-	};
+    out = wrapAnsi(out, getWidth(stream), {
+      trim: false,
+      hard: true,
+      wordWrap: false
+    });
+    stream.write(ansiEscapes.eraseLines(prevLineCount) + out);
+    prevLineCount = out.split('\n').length;
+  };
 
-	render.done = () => {
-		prevLineCount = 0;
+  render.clear = () => {
+    stream.write(ansiEscapes.eraseLines(prevLineCount));
+    prevLineCount = 0;
+  };
 
-		if (!options.showCursor) {
-			cliCursor.show();
-		}
-	};
+  render.done = () => {
+    prevLineCount = 0;
 
-	return render;
+    if (!options.showCursor) {
+      cliCursor.show();
+    }
+  };
+
+  return render;
 };
 
 module.exports = main(process.stdout);
-// TODO: Remove this for the next major release
 module.exports.default = module.exports;
 module.exports.stderr = main(process.stderr);
 module.exports.create = main;
